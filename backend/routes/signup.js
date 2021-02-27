ef544fb4aca225a7b5b9ba4ace9035ab9dae1530
Sushant002001 <capricorn.sushant@gmail.com>
2021-02-27 02:48:32 -0800

    signup logic

---
+++ backend/routes/signup.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const pool = require('../pool.js');
+
+
+router.post('/', (req, res) => {
+  console.log('Inside Signup Post Request');
+  console.log('Req Body : ', req.body);
+
+  let sql = `CALL post_signup('${req.body.email_id}','${req.body.password}','${req.body.username}');`;
+
+  pool.query(sql,(err, result) =>{
+    if(err){
+      res.writeHead(500, {
+        'Content-Type': 'text/plain'
+        });
+        res.send(err);
+    }
+    if(result && result.length > 0 && result[0][0].status==1){
+        res.writeHead(200, {
+          'Content-Type': 'text/plain'
+        })
+        res.end("NEW USER CREATED");
+      }
+    else if(result && result.length > 0 && result[0][0].status==0){
+        res.writeHead(401, {
+          'Content-Type': 'text/plain'
+        })
+        res.end("USER ALREADY EXISTS");
+      }
+  })
+});
+
+module.exports = router;
